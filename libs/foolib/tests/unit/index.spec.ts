77de17e1c48e427b9f7be0e2389ea18be06722aa
Andrew Ellerton <andrew.j.ellerton@gmail.com>
2021-05-01 15:09:30 +0800

    basic eslint+typescript support added

--- libs/foolib/tests/unit/index.spec.ts
+++ libs/foolib/tests/unit/index.spec.ts
@@ -1,13 +1,13 @@
-import {expect} from 'chai'
-import {timestamp} from "../../src"
+import { expect } from 'chai'
+import { timestamp } from '../../src'
 
 describe('basic test', () => {
-    it('timestamp date in the expected format', () => {
-        const r = timestamp()
-        expect(r).to.match(/^\d\d\d\d-\d\d-\d\d/)
-    })
-    it('timestamp fully in the expected format', () => {
-        const r = timestamp()
-        expect(r).to.match(/^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d\.\d\d\dZ$/)
-    })
+  it('timestamp date in the expected format', () => {
+    const r = timestamp()
+    expect(r).to.match(/^\d\d\d\d-\d\d-\d\d/)
+  })
+  it('timestamp fully in the expected format', () => {
+    const r = timestamp()
+    expect(r).to.match(/^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d\.\d\d\dZ$/)
+  })
 })
